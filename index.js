dcf3f3082b15e3056c4b477c1432ac8e0613578c
Gustavo Vilela <87400905+Gus-Vilela@users.noreply.github.com>
2023-05-24 02:42:58 +0000

    Update 3 files, add database connection and schemas

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URI);
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date,
+});
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  _id: {
+    type: String,
+    unique: true,
+  },
+});
 
+const logSchema = new mongoose.Schema({
+  username: String,
+  count: Number,
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: Date,
+    },
+  ],
+});
 
+let User = mongoose.model("User", userSchema);
+
+let user = new User({
+  username: "RedH",
+  _id: "exampleid",
+});
+
+user.save();
+
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+let exercise = new Exercise({
+  username: "RedH",
+  description: "Running",
+  duration: 60,
+  date: new Date(),
+});
+
+exercise.save();
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
